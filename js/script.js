52889bb429c2690144522d7625268307f8d32177
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-28 14:40:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        imageUrl: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        imageUrl: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        imageUrl: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        imageUrl: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        imageUrl: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        imageUrl: 'barbara-ramos-graphic-designer.jpg'
+    },
+]
